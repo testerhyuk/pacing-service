6a3ea4034993a3153ef1022079b059ceb3acb7fa
Hyuk <testerhyuk214@gmail.com>
2026-07-21 14:31:55 +0900

    chore: initialize pacing service project

---
+++ src/main/java/com/settlement/pacing_service/PacingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.settlement.pacing_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/settlement/pacing_service/PacingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.settlement.pacing_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PacingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
